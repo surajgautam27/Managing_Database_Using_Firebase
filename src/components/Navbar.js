d8bb670a6d9cde5ab01f7b399e6805d09032dbb6
Suraj <surajgautam1111@gmai;l.com>
2023-03-02 22:26:19 +0545

    last commit

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -3,16 +3,27 @@ import React from 'react'
 import {Link} from "react-router-dom"
 import {auth} from "../config/firebase.ts"
 import {useAuthState} from 'react-firebase-hooks/auth'
+import { signOut } from 'firebase/auth'
 function Navbar() {
 const [user] =useAuthState(auth);
+
+const signUserOut =async () =>{
+  await signOut(auth);
+
+}
   return (
     <div>
      <Link to='/'>Home</Link><br/>
      <Link to='/login'>Login page</Link>
      <div>
+  {user &&(   <>
+    <p>{user?.dislayName}</p>
+     <img alt="apple" src={user.photoURL}/> <br></br>
+     <button onClick={signUserOut}>Sign Out</button>
+  </>
+     
+     )}
   
-      <p>{user?.dislayName}</p>
-     <img alt="ap" src={ user.photoURL }/>
      </div>
     </div>
   )
